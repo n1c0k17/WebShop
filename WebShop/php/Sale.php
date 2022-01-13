78ba892e212b564efa84bd51128c139b4decdfa1
Nico Knöfel <nicolas.knoefel@web.de>
2022-01-13 12:23:18 +0100

    Fehlerbehebungen

--- WebShop/php/Sale.php
+++ WebShop/php/Sale.php
@@ -6,40 +6,43 @@
 <head>
 	<meta charset="utf-8"/>
 	<meta name="Staubsauger Webshop" content="Staubsauger, vacuum cleaner"/>
-	<link rel=" stylesheet" href="style.css">
+	<link rel=" stylesheet" href="../css/style.css">
 	<title>Sale</title>
 </head>
 <body>
 <div id="top">
-		<div id="name">World of Staubsauger</div>
-		<div id="Logo">WoS</div>
-		<div id="Slogan">"Für ein sauberes Zuhause"</div>
-			<a href="WebShop.php">start</a>
-			<a href="besteller.php">Sale</a>
-			<a href="Impressum.php">Impressum</a>
-			<a href="#"><form action="input.htm">
-							<label>suchen:<input type="search" list = "Produktkatalog"></label>
-							<datalist id = "Produktkatalog">
-							<option value = "Krümel-Collector">
-							<option value = "500W">
-							<option value = "Kabelgebunden">
-							<option value = "Kabellos">
-							<option value = "Schwarz">
-							<option value = "85dB">
-							<datalist>
-							<input type= "submit">
-							</form>
-							</a>
-			<a href="#">Blitzangebote</a>
-			<a href="#">Angebote unter 20€</a>
-			<a href="#">Bundle</a>
-<div/>
+	<div id="name">World of Staubsauger</div>
+	<div id="Logo">WoS</div>
+	<div id="Slogan">"Für ein sauberes Zuhause"</div>
+	<div id="navigation">
+		<a href="index.php">start</a>
+		<a href="besteller.php">Sale</a>
+		<a href="Impressum.php">Impressum</a>
+		<a href="#"><form action="input.htm">
+						<label>suchen:<input type="search" list = "Produktkatalog"></label>
+						<datalist id = "Produktkatalog">
+						<option value = "Krümel-Collector">
+						<option value = "500W">
+						<option value = "Kabelgebunden">
+						<option value = "Kabellos">
+						<option value = "Schwarz">
+						<option value = "85dB">
+						<datalist>
+						<input type= "submit">
+						</form>
+						</a>
+		<a href="#">Blitzangebote</a>
+		<a href="#">Angebote unter 20€</a>
+		<a href="#">Bundle</a>
+
+	</div>	
+</div>
 <div class="articel">
 <div id="image Krümel-Collector">
 			<img src="Krümelcollector V1.jpg" alt="Bild vom Krümel-Collector">
-		<div/>
+</div>
 <a href="Artikel.html">krümel-Collector</a>
-<div/>
+</div>
 <body>
 <footer>
 		<div id="Beschreibung">Ein Projekt der Otto von Guericke Universität</div>
